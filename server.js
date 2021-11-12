e06ac3c693a4b482c0bc21636c1c6e783d36507a
Ethan C <ethan.cho1826@gmail.com>
2021-11-11 22:23:53 -0600

    added frontend routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const routes = require('./controllers');
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,8 +18,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+app.use(routes);
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/exercise.html'));
+});
+
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/stats.html'));
+});
 // Start the server
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
